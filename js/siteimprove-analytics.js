5dded82da9c9f1f6c08dfb29c193ca0dea299273
rvdforst <rolf.v.d.forst@fau.de>
2022-03-07 09:27:47 +0100

    Delete siteimprove-analytics.js

--- js/siteimprove-analytics.js
+++
@@ -1,5 +0,0 @@
-(function() {
-var sz = document.createElement('script'); sz.type = 'text/javascript'; sz.async = true;
-sz.src = 'https://siteimproveanalytics.com/js/siteanalyze_' + siteanalyze.code + '.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s);
-})();
